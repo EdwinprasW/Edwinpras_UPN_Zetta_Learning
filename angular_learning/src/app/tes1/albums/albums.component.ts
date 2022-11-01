343a1918e5a3203fca26dfc46af46e1c225599f2
EdwinprasW <weather2402@gmail.com>
2022-11-02 01:13:52 +0700

    Almost Finishing Day 2

--- angular_learning/src/app/tes1/albums/albums.component.ts
+++ angular_learning/src/app/tes1/albums/albums.component.ts
@@ -1,11 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+export interface albumList{
+  name: string,
+  ratingOutOfTen: number,
+  artist : string
+}
+
+export interface LikedAlbums{
+  name: string,
+  ratingOutOfTen: number,
+  artist : string
+}
+
+
 
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
   styleUrls: ['./albums.component.css']
 })
+
 export class AlbumsComponent implements OnInit {
+
+  @Output ('albumLiked') onlikeAlbum : EventEmitter<albumList> = new EventEmitter<albumList>()
+
+  public theAlbums: albumList[] = [
+    {name: 'Straight Outta Compton', ratingOutOfTen: 9, artist : 'NWA'},
+    {name: 'Vessel', ratingOutOfTen: 8, artist : 'Twenty One Pilots'},
+    {name: 'Deviltown', ratingOutOfTen: 7,  artist : 'Cavetown'},
+    {name: 'Trench', ratingOutOfTen: 10,  artist : 'Twenty One Pilots'},
+    {name: 'Slim Shady LP', ratingOutOfTen: 9,  artist : 'Eminem'},
+  ]
+
+  public likedAlbum : LikedAlbums[]=[]
+
+  likeAlbum(theAlbums : albumList){
+    this.onlikeAlbum.emit(theAlbums)
+    this.likedAlbum.push({...theAlbums})
+  }
 
   constructor() { }
 
